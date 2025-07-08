d485d20d66c3c54b6a45faeea51919b74bcfc86a
STSumu <sabihatahsinsumu@gmail.com>
2025-07-08 22:09:46 +0600

    added details page

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,61 +1,93 @@
 import { useContext } from "react";
-import { FaArrowLeft, FaStar } from "react-icons/fa";
+import { FaArrowLeft, FaShoppingCart } from "react-icons/fa";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import { authContext } from "../context/AuthProvider";
 import Rating from "./Rating";
+import { MdUpdate } from "react-icons/md";
+
 
 const CourseDetails = () => {
-  const params=useParams();
-  const course=useContext(authContext).courses.find((course)=>course.course_name===params.course_name);
-  console.log(course);
-    const {course_name,category,instructor,duration,price,thumb_url,instructorImg,description,rating,totalstudent}=course;
-    const handleAddCart=()=>{
-        fetch(`${baseUrl}/cart/${course_name}`, {
-                  method: "POST",
-                  headers: {
-                    "content-type": "application/json",
-                  },
-                  body: JSON.stringify(loadedData),
-                })
-        .then((res) => res.json())
-        .then((data) => {
-                    if (data.insertedId) {
-                       alert('success')
-                    }
-                  });
-    }
+  const { baseUrl } = useContext(authContext);
+  const params = useParams();
+  const course = useContext(authContext).courses.find((course) => course.course_name === params.course_name);
+  const { course_name, course_id, category, status, instructor, duration, price, updated_at, thumb_url, instructorImg, description, rating, totalstudent } = course;
+  const date = new Date(updated_at);
+  const month = date.getMonth() + 1; // getMonth() is 0-indexed
+  const year = date.getFullYear();
+  const handleAddCart = () => {
+    fetch(`${baseUrl}/cart/${course_name}`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(course_id),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert('success')
+        }
+      });
+  }
+  const handleAddWishList = () => {
+    fetch(`${baseUrl}/wish/${course_name}`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(course_id),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert('success')
+        }
+      });
+  }
   return (
-    <div className="mt-10">
-      <div className="p-4 md:p-10 md:px-25 bg-[#471608] text-white flex justify-center flex-col">
-          <p>{category}</p>
-          <h1 className="text-5xl font-bold">{course_name}</h1>
-          <p className="py-6">
-        {description}
-      </p>
-      {/* second section  */}
-          <div className="flex gap-2 items-center w-full lg:w-1/3">
+    <div className="mt-15 md:mt-10">
+      <div className="p-4 md:p-10 md:px-25 bg-[#471608] text-white flex justify-center flex-col md:mb-20">
+        <p>{category}</p>
+        <h1 className="text-5xl font-bold">{course_name}</h1>
+        <p className="py-6">
+          {description}
+        </p>
+        {/* second section  */}
+        <div className="flex gap-2 items-center w-full lg:w-1/3">
+          <button className="btn btn-xs border-0 rounded-lg shadow-none bg-[#D8BFAA] font-bold">{status.charAt(0).toUpperCase() + status.slice(1)}</button>
+          <p className="inline text-yellow-400 font-bold">{rating ? Number(rating).toFixed(1) : 'N/A'}</p>
+          <Rating rating={rating}></Rating>
+          <p className="text-sm"> {totalstudent} students</p>
+        </div>
 
-            <p className="inline">{rating ? Number(rating).toFixed(1) : 'N/A'}</p>
-            <Rating rating={rating}></Rating>
-<p>{totalstudent} Students</p>
-          </div>
-      
+        <p className="text-sm py-4 ">Created by <Link className="text-[#f3a574] underline">{instructor}</Link></p>
+        <div className="flex items-center">
+          <MdUpdate /><p>Last updated {month}/{year}</p>
+        </div>
+
       </div>
-      <div className="hero bg-base-200 md:min-h-screen w-full">
-  <div className="hero-content flex-col lg:flex-row">
-    <div className="w-1/2">
-      <img
-    src={thumb_url}
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
-    </div>
-    <div>
-      
-      <button className="btn" onClick={handleAddCart}>Add to cart</button>
-      <button className="btn btn-primary">Add to cart</button>
-    </div>
-  </div>
-</div>
+      <div className="card bg-base-100 my-4 mx-auto md:my-0 w-full md:w-1/3 lg:w-1/4 shadow-sm md:absolute md:top-50 md:right-30 lg:right-40">
+        <figure className="pt-6 md:pt-0">
+          <img className="object-cover"
+            src={thumb_url}
+            alt={course_name} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            ${price}
+            <div className="badge badge-secondary bg-[#D8BFAA] text-black border-0">{status}</div>
+          </h2>
+
+          <div className="card-actions flex flex-col">
+            <button className="btn text-white bg-[#B14E0F] w-full" onClick={handleAddCart}><FaShoppingCart />Add to Cart</button>
+            <button className="btn text-black border-[#B14E0F] w-full" onClick={handleAddWishList}>Add to WishList</button>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto px-4 md:px-10 lg:px-25">
+        <h3 className="font-bold text-3xl">This Course includes:</h3>
+
+      </div>
     </div>
   );
 };
