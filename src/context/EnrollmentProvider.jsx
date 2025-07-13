2e12f5ca41344457e319dfab897f751fa69b62d0
Tasnia2742 <tasnialamisha@gmail.com>
2025-07-13 19:12:37 +0600

    modified enrolled

--- src/context/EnrollmentProvider.jsx
+++ src/context/EnrollmentProvider.jsx
@@ -2,7 +2,7 @@ import React, { Children, createContext, useEffect, useState } from 'react';
 
 const EnrollContext=createContext();
 const EnrollmentProvider = ({children}) => {
-    const [enLoading,setEnLoading]=useState();
+    
     useEffect(()=>{
 },[])
     const enroll={
